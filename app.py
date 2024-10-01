fe33ed32c64a03f00fbf04654bb28ae4699bf7ec
Fabinho <20231ITA01GB0011@alunos.ifbaiano.edu.br>
2024-10-01 13:44:44 -0300

    Update

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ import smtplib
 import os
 from dotenv import load_dotenv
 
-# Carrega variaveis de ambiente do arquivo .env
+# Carrega variáveis de ambiente do arquivo .env
 load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = 'chave_secreta_para_flash'  # Necessário para o uso do flash messages
 
-# Carrega as variaveis de e-mail e senha do arquivo .env
+# Carrega as variáveis de e-mail e senha do arquivo .env
 EMAIL_ADDRESS = os.getenv('EMAIL_USER')
 EMAIL_PASSWORD = os.getenv('EMAIL_PASS')
 
@@ -17,25 +17,32 @@ EMAIL_PASSWORD = os.getenv('EMAIL_PASS')
 def index():
     if request.method == 'POST':
         suggestion = request.form['suggestion']
-        
-        # Envia a sugestao via e-mail
-        send_email(suggestion)
-        
-        # Exibe a mensagem de sucesso
-        flash('Sua sugestao foi enviada com sucesso!', 'success')
-        return redirect(url_for('index'))
-    
+
+        # Verifica se a sugestão não está vazia
+        if suggestion:
+            # Envia a sugestão via e-mail
+            send_email(suggestion)
+
+            # Exibe a mensagem de sucesso
+            flash('Sua sugestão foi enviada com sucesso!', 'success')
+            return redirect(url_for('index'))
+        else:
+            flash('Por favor, insira uma sugestão.', 'danger')
+
     return render_template('index.html')
 
 def send_email(suggestion):
+    # Garantindo que a sugestão seja tratada como string UTF-8
+    suggestion_utf8 = suggestion.encode('utf-8', errors='ignore').decode('utf-8')
+
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
         smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
-        
-        subject = 'Nova Sugestao Anonima'
-        body = f'Sugestao recebida: {suggestion}'
-        
+
+        subject = 'Nova Sugestão Anônima'
+        body = f'Sugestão recebida: {suggestion_utf8}'
+
         msg = f'Subject: {subject}\n\n{body}'
-        
+
         smtp.sendmail(EMAIL_ADDRESS, EMAIL_ADDRESS, msg)
 
 if __name__ == "__main__":
